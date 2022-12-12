5f14a5437a3c5df7963aa50cfc3cec2e6b1dff7b
spnikit <spnikit@gmail.com>
2022-12-12 21:56:14 +0300

    add async validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppComponent} from './app.component';
 import {TodoComponent} from './components/todo/todo.component';
 import {TodoListComponent} from './components/todo-list/todo-list.component';
 import {NewTodoComponent} from './components/new-todo/new-todo.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SearchTodoComponent} from './components/search-todo/search-todo.component';
 import {MaterialModule} from "./material/material.module";
 import {AuthComponent} from './components/auth/auth.component';
@@ -17,8 +17,8 @@ import {ENVIRONMENT} from "./services/environment.service";
 import {environment} from "../environments/environment";
 import {AuthGuard} from "./guards/auth.guard";
 import {TokenInterceptor} from "./interceptors/token.interceptor";
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MapPathToMenuNamePipe } from './pipes/map-path-to-menu-name.pipe';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {MapPathToMenuNamePipe} from './pipes/map-path-to-menu-name.pipe';
 
 export const routes: Routes = [
   {path: "main", component: TodoMainComponent},
@@ -43,6 +43,7 @@ export const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
     MaterialModule,
     HttpClientModule
